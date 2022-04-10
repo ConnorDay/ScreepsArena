232e1f0b0c95b80b464cd711e9fe5c7152d2f60d
dsiher <david.siher@gmail.com>
2022-04-10 15:18:04 -0400

    Hotfix basecreep directory

--- src/tower.ts
+++ src/tower.ts
@@ -1,117 +1,117 @@
 import { AnyCreep, ResourceConstant, ScreepsReturnCode } from "game/constants";
 import { Store, Structure, StructureTower } from "game/prototypes";
 import { findClosestByPath, findClosestByRange, getRange } from "game/utils";
-import { BaseCreep } from "./basecreep";
+import { BaseCreep } from "./roles/basecreep";
 import { World } from "./world";
 
 class Tower {
-	private _primitiveTower: StructureTower;
-	constructor(tower: StructureTower) {
-		this._primitiveTower = tower;
-	}
+    private _primitiveTower: StructureTower;
+    constructor(tower: StructureTower) {
+        this._primitiveTower = tower;
+    }
 
-	/**
-	 * Priority:
-	 *  1. Attack any enemy creep that's within 5 tiles of the flag
-	 *  2. Heal any allied creep that's within 50 tiles of the tower and while energy is > 50%
-	 *  3. Attack any enemy creep that's within 50 tiles and while energy == 100%?
-	 */
-	public run() {
-		const close: BaseCreep[] = [];
-		const targets: BaseCreep[] = [];
+    /**
+     * Priority:
+     *  1. Attack any enemy creep that's within 5 tiles of the flag
+     *  2. Heal any allied creep that's within 50 tiles of the tower and while energy is > 50%
+     *  3. Attack any enemy creep that's within 50 tiles and while energy == 100%?
+     */
+    public run() {
+        const close: BaseCreep[] = [];
+        const targets: BaseCreep[] = [];
 
-		//Populate $close and $targets
-		World.enemies.forEach((c) => {
-			const dist = getRange(this, c);
-			if (dist <= 5) {
-				close.push(c);
-			} else if (dist <= 50) {
-				//Should $targets also contain all of $close?
-				targets.push(c);
-			}
-		});
+        //Populate $close and $targets
+        World.enemies.forEach((c) => {
+            const dist = getRange(this, c);
+            if (dist <= 5) {
+                close.push(c);
+            } else if (dist <= 50) {
+                //Should $targets also contain all of $close?
+                targets.push(c);
+            }
+        });
 
-		//Priority 1
-		if (close.length > 0) {
-			const target = findClosestByPath(World.myFlag, close);
-			this.attack(target);
-			return;
-		}
+        //Priority 1
+        if (close.length > 0) {
+            const target = findClosestByPath(World.myFlag, close);
+            this.attack(target);
+            return;
+        }
 
-		//Priority 2
-		if ((this.store.getUsedCapacity("energy") as number) >= 40) {
-			const target = targets.reduce((prev, curr) => {
-				//I need the getDanger function before this can function
-				return prev;
-			}, World.allies[0]);
-		}
-		//Add the check that the creep is actually in danger here
+        //Priority 2
+        if ((this.store.getUsedCapacity("energy") as number) >= 40) {
+            const target = targets.reduce((prev, curr) => {
+                //I need the getDanger function before this can function
+                return prev;
+            }, World.allies[0]);
+        }
+        //Add the check that the creep is actually in danger here
 
-		//Priority 3
-		if (this.store.getUsedCapacity("energy") === 50) {
-			const target = findClosestByRange(this, targets);
-			this.attack(target);
-			return;
-		}
+        //Priority 3
+        if (this.store.getUsedCapacity("energy") === 50) {
+            const target = findClosestByRange(this, targets);
+            this.attack(target);
+            return;
+        }
 
-		return; // c:
-	}
+        return; // c:
+    }
 
-	public getDamageToTarget(target: AnyCreep | Structure): number {
-		const dist = getRange(this, target);
-		if (dist <= 5) {
-			return 600;
-		} else if (dist > 50) {
-			return 0;
-		} else if (dist >= 20) {
-			return 150;
-		}
+    public getDamageToTarget(target: AnyCreep | Structure): number {
+        const dist = getRange(this, target);
+        if (dist <= 5) {
+            return 600;
+        } else if (dist > 50) {
+            return 0;
+        } else if (dist >= 20) {
+            return 150;
+        }
 
-		return 600 - (dist / 15) * 450;
-	}
+        return 600 - (dist / 15) * 450;
+    }
 
-	/////////////////////////////////
-	// Mappings to primitive tower //
-	/////////////////////////////////
+    /////////////////////////////////
+    // Mappings to primitive tower //
+    /////////////////////////////////
 
-	//Getters//
-	public get my(): boolean {
-		return this._primitiveTower.my;
-	}
+    //Getters//
+    public get my(): boolean {
+        return this._primitiveTower.my;
+    }
 
-	public get hits(): number {
-		return this._primitiveTower.hits;
-	}
+    public get hits(): number {
+        return this._primitiveTower.hits;
+    }
 
-	public get hitsMax(): number {
-		return this._primitiveTower.hitsMax;
-	}
+    public get hitsMax(): number {
+        return this._primitiveTower.hitsMax;
+    }
 
-	public get x(): number {
-		return this._primitiveTower.x;
-	}
+    public get x(): number {
+        return this._primitiveTower.x;
+    }
 
-	public get y(): number {
-		return this._primitiveTower.y;
-	}
+    public get y(): number {
+        return this._primitiveTower.y;
+    }
 
-	//Get rid of this entirely? just replace with energy/ max energy/ missing energy?
-	public get store(): Store<ResourceConstant> {
-		return this._primitiveTower.store;
-	}
+    //Get rid of this entirely? just replace with energy/ max energy/ missing energy?
+    public get store(): Store<ResourceConstant> {
+        return this._primitiveTower.store;
+    }
 
-	public get cooldown(): number {
-		return this._primitiveTower.cooldown;
-	}
+    public get cooldown(): number {
+        return this._primitiveTower.cooldown;
+    }
 
-	//functions//
-	public attack(target: AnyCreep | Structure): ScreepsReturnCode {
-		return this._primitiveTower.attack(target);
-	}
+    //functions//
+    public attack(target: AnyCreep | Structure): ScreepsReturnCode {
+        return this._primitiveTower.attack(target);
+    }
 
-	public heal(target: AnyCreep): ScreepsReturnCode {
-		return this._primitiveTower.heal(target);
-	}
+    public heal(target: AnyCreep): ScreepsReturnCode {
+        return this._primitiveTower.heal(target);
+    }
 }
 
 export { Tower };
--- src/world.ts
+++ src/world.ts
@@ -1,23 +1,23 @@
 import { Flag } from "arena";
 import { Creep, StructureTower } from "game/prototypes";
 import { getObjectsByPrototype } from "game/utils";
-import { BaseCreep } from "./basecreep";
+import { BaseCreep } from "./roles/basecreep";
 import { Tower } from "./tower";
 
 class worldClass {
-	allies: BaseCreep[] = [];
-	enemies: BaseCreep[] = [];
-	myTowers: Tower[] = [];
-	enemyTowers: Tower[] = [];
-	myStoredFlag: Flag | undefined;
-	enemyStoredFlag: Flag | undefined;
+    allies: BaseCreep[] = [];
+    enemies: BaseCreep[] = [];
+    myTowers: Tower[] = [];
+    enemyTowers: Tower[] = [];
+    myStoredFlag: Flag | undefined;
+    enemyStoredFlag: Flag | undefined;
 
-	public get myFlag(): Flag {
-		return this.myStoredFlag as Flag;
-	}
-	public get enemyFlag(): Flag {
-		return this.enemyStoredFlag as Flag;
-	}
+    public get myFlag(): Flag {
+        return this.myStoredFlag as Flag;
+    }
+    public get enemyFlag(): Flag {
+        return this.enemyStoredFlag as Flag;
+    }
 }
 
 const World = new worldClass();
