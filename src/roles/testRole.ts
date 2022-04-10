fa1af1ae3e4745d9f68b2d0177f05070f8168899
Connor Day <connor.t.day@gmail.com>
2022-04-10 14:05:47 -0400

    commit

---
+++ src/roles/testRole.ts
@@ -0,0 +1,11 @@
+import { BaseCreep } from "../basecreep";
+
+export class TestRole extends BaseCreep {
+	public run(): void {
+		this.speak();
+	}
+
+	private speak(): void {
+		console.log("i'm a private test creep :)");
+	}
+}
