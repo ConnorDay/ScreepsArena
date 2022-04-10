d629e890fe97a6bb9ac3800b2d72a1ab98fd9bed
Connor Day <connor.t.day@gmail.com>
2022-04-10 14:43:16 -0400

    support for multiple roles

--- src/roles/assign.ts
+++ src/roles/assign.ts
@@ -2,12 +2,24 @@ import { Creep } from "game/prototypes";
 import { BaseCreep } from "../basecreep";
 import { TestRole } from "./testRole";
 
-const roster = [
-	{
-		role: BaseCreep,
-	},
+interface rosterEntry {
+    role: any;
+    count: number;
+}
+
+const roster: rosterEntry[] = [
+    {
+        role: TestRole,
+        count: 7,
+    },
 ];
 
 export function assignCreep(creep: Creep): BaseCreep {
-	return new TestRole(creep);
+    for (let entry of roster) {
+        if (entry.count > 0) {
+            entry.count--;
+            return new entry.role(creep);
+        }
+    }
+    return new BaseCreep(creep);
 }
